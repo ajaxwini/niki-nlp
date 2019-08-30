87c4470306b264a3d59114f61be05bff83350667
AjayKrPrajapati <ajay.prajapati@smecorner.com>
2019-08-30 17:56:12 +0530

    part 2

--- nikiSolution1.py
+++ nikiSolution1.py
@@ -77,7 +77,7 @@ Y = train_raw['Star Rating']
 x_train, x_valid, y_train, y_valid = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 x_test = all_reviews_test
-y_test = test_raw['Star Rating']
+# y_test = test_raw['Star Rating']
 
 vectorizer = TfidfVectorizer(ngram_range=(1,2))
 
@@ -93,3 +93,57 @@ x_valid_vectors = selector.transform(x_valid_vectors).astype('float32')
 x_test_vectors = selector.transform(x_test_vectors).astype('float32')
 
 round(train_raw['Star Rating'].value_counts() * 10 / train_raw.shape[0])
+
+few_classes = list(map(lambda x: x in list(range(2, 5)), y_train.tolist()))
+
+more_classes = list(map(lambda x: x in [1, 5], y_train.tolist()))
+
+np.sum(few_classes)
+
+few_classes = np.array(few_classes)
+more_classes = np.array(more_classes)
+
+x_train_few = x_train_vectors[few_classes]
+y_train_few = y_train[few_classes]
+
+x_train_more = x_train_vectors[more_classes]
+y_train_more = y_train[more_classes]
+
+all_x_train = list(x_train_few)
+all_y_train = list(y_train_few)
+
+for i in range(2):
+    all_x_train.extend(x_train_few)
+    all_y_train.extend(y_train_few)
+
+all_x_train.extend(x_train_more)
+all_y_train.extend(y_train_more)
+
+
+all_x_train = np.concatenate([x_train_more.toarray(), x_train_few.toarray(), x_train_few.toarray(), x_train_few.toarray()], axis=0)
+all_y_train = np.concatenate([y_train_more, y_train_few, y_train_few, y_train_few], axis=0)
+
+all_y_train.shape
+
+new_x_train, new_y_train = shuffle(all_x_train, all_y_train)
+
+new_x_train.shape
+
+##MODEL
+
+clf = ExtraTreesClassifier(n_estimators=100, random_state=0, class_weight='balanced')
+
+clf.fit(x_train_vectors, y_train)
+print('Accuracy of classifier on training set: {:.2f}'.format(clf.score(x_train_vectors, y_train) * 100))
+# print('Accuracy of classifier on test set: {:.2f}'.format(clf.score(x_test_vectors, y_test) * 100))
+
+y_pred = clf.predict(x_test_vectors) ##RESULTS
+# precision_score(y_test, y_pred, average='macro')
+
+
+
+
+
+# MLPClassifier
+
+
